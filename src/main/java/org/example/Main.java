d9c758389fd82098c1b519c35cc20e2d20f47c03
oowgnus <sungwoo9027@naver.com>
2025-09-17 17:28:39 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!!");
+        System.out.println("민지호야 나만 빼고 술 먹으니 행복하니 정말 서운하다");
     }
 }
